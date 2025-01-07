0398519bc342bb246202ef9741bb845e65b9edd4
Енот <jekserv@gmail.com>
2025-01-07 23:08:23 +0300

    V0.2

---
+++ задание 2/zd1.py
@@ -0,0 +1,55 @@
+import asyncio
+import sqlite3
+
+from faker import Faker
+
+from main import *
+
+faker = Faker()
+
+
+async def input_db():
+    """
+    Input data to database
+    """
+    if not await db.create_tables():
+        await db.create_tables()
+    else:
+        print("Tables already exist")
+
+
+    users = [
+        (faker.name(), faker.email(),
+         faker.random_int(min=18, max=100),
+         faker.random_int(min=0, max=1000))
+        for _ in range(10)  # Замените N на нужное количество пользователей
+    ]
+
+    for user, email, age, balance in users:
+        await db.add_input(user, email, age, balance)
+        await db.update_db()
+
+    await db.delete_db()
+    await db.get_balance()
+    bal = await db.get_users()
+    sum = await db.get_osall()
+    print(f'{bal}/{sum}={bal/sum:.2f}')
+
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
+
+# Выполнение запроса
+cursor.execute("SELECT username, age FROM users WHERE age != 60")
+results = cursor.fetchall()
+
+
+# Вывод записей в консоль
+for row in results:
+    username, age = row
+    print(f"Имя: {username} | Возраст: {age}")
+
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(input_db())
